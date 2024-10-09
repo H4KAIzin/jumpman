2befb2dbd636929562ed6ccfa5c79297104977d4
luquitas <viniciaosantosdacosta37378@gmail.com>
2024-10-09 08:22:54 -0300

    Wall

---
+++ pintopulante/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace pintopulante;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ pintopulante/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace pintopulante;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
