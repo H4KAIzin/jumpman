eafcc806651468a40942e8e916f10619dd6d98fa
luquitas <viniciaosantosdacosta37378@gmail.com>
2024-10-15 08:56:28 -0300

    Bellator

--- pintopulante/GamePage.xaml.cs
+++ pintopulante/GamePage.xaml.cs
@@ -2,7 +2,7 @@ namespace pintopulante;
 
 public partial class GamePage : ContentPage
 {
-    const int Gravidade = 1;
+    const int Gravidade = 2;
     const int tempoEntreFrames = 25;
     bool estaMorto = false;
     double LarguraJanela = 3;
@@ -27,6 +27,13 @@ public partial class GamePage : ContentPage
             AplicaGravidade();
             await Task.Delay(tempoEntreFrames);
             GerenciaCanos();
+            if(VerificaColisao())
+            {
+                estaMorto = true;
+                frameGameOver.IsVisible = true;
+                break;
+            }
+            await Task.Delay(tempoEntreFrames); 
         }
     }
 
@@ -58,5 +65,37 @@ public partial class GamePage : ContentPage
             imgcanobaxo.TranslationX = 0;
             imgcanocima.TranslationX = 0;
         }
+    }
+
+    bool VerificaColisao()
+    {
+        if(!estaMorto)
+        {
+            if(VerificaColisaoTeto()|| 
+            VerificaColisaoChao())
+               {
+                return true;
+               }
+        }
+                return false;
+    }
+
+    bool VerificaColisaoTeto()
+    {
+        var minY =- AlturaJanela/2;
+        if(imgJunin.TranslationY <= minY)
+            return true;
+        else
+            return false;
+    }
+
+    bool VerificaColisaoChao()
+    {
+        var maxY = AlturaJanela/2 - imglauva.HeightRequest -imgJunin.HeightRequest;
+        if(imgJunin.TranslationY >= maxY)
+            return true;
+        else
+            return false;
+
     }
 }
